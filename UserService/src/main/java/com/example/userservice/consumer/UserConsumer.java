a34ec4d74e0966931ef7312a2765b710442b5861
ilgiz <i.samudinov@outlook.com>
2025-11-04 12:50:25 +0600

    feat(user-service): add UserConsumer to handle user messages and create profiles

--- UserService/src/main/java/com/example/userservice/consumer/UserConsumer.java
+++ UserService/src/main/java/com/example/userservice/consumer/UserConsumer.java
@@ -1,4 +1,18 @@
 package com.example.userservice.consumer;
 
+import com.example.userservice.dto.UserResponse;
+import com.example.userservice.service.UserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+@Component
+@RequiredArgsConstructor
 public class UserConsumer {
+
+    private final UserProfileService userProfileService;
+
+    @RabbitListener(queues = "#{rabbitMqProperties.queue}")
+    public void handleUserMessage(UserResponse userResponse) {
+        userProfileService.createUserProfile(userResponse);
+    }
 }
