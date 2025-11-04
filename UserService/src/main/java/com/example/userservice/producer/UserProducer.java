ba6db46a2f874fb99dafb3ae0e6df5c8bfae5a01
ilgiz <i.samudinov@outlook.com>
2025-11-04 12:52:41 +0600

    feat(user-service): add UserProducer to send user messages via RabbitMQ

--- UserService/src/main/java/com/example/userservice/producer/UserProducer.java
+++ UserService/src/main/java/com/example/userservice/producer/UserProducer.java
@@ -1,4 +1,19 @@
 package com.example.userservice.producer;
 
+import com.example.userservice.config.RabbitMqProperties;
+import com.example.userservice.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final RabbitMqProperties rabbitMqProperties;
+
+    public void send (UserResponse userResponse) {
+        rabbitTemplate.convertAndSend(rabbitMqProperties.getExchange(), rabbitMqProperties.getRoutingKey(), userResponse);
+    }
 }
