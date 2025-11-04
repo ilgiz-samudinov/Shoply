6355676c01b94312275374f7674d02a2c42ceacd
ilgiz <i.samudinov@outlook.com>
2025-11-04 12:48:41 +0600

    refactor(user-service): make firstName and lastName nullable in UserProfile entity

--- UserService/src/main/java/com/example/userservice/model/UserProfile.java
+++ UserService/src/main/java/com/example/userservice/model/UserProfile.java
@@ -16,10 +16,8 @@ public class UserProfile {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String firstName;
 
-    @Column(nullable = false)
     private String lastName;
 
 
