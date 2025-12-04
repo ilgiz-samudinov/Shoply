d0ba65add09a23d1788dadbac9a457b021f2dd6b
ilgiz <i.samudinov@outlook.com>
2025-12-04 15:33:53 +0600

    refactor(product-service): remove comments from ProductRequest

--- ProductService/src/main/java/com/example/productservice/dto/ProductRequest.java
+++ ProductService/src/main/java/com/example/productservice/dto/ProductRequest.java
@@ -10,13 +10,13 @@ import java.math.BigDecimal;
 @Data
 public class ProductRequest {
 
-    @NotBlank(message = "Title is required")  // не пустая строка и не null
+    @NotBlank(message = "Title is required")
     private String title;
 
-    @NotBlank(message = "Description is required")  // не пустая строка и не null
+    @NotBlank(message = "Description is required")
     private String description;
 
-    @NotNull(message = "Price is required")       // не null
-    @Min(value = 0, message = "Price must be >= 0") // минимальное значение
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price must be >= 0")
     private BigDecimal price;
 }
