ce302c834c54d8dc217218ad48d8b97b3f3dc14e
ilgiz <i.samudinov@outlook.com>
2025-12-04 18:57:56 +0600

    feat(product-service): integrate ProductProducer for messaging

--- ProductService/src/main/java/com/example/productservice/service/ProductService.java
+++ ProductService/src/main/java/com/example/productservice/service/ProductService.java
@@ -5,32 +5,37 @@ import com.example.productservice.dto.ProductRequest;
 import com.example.productservice.exception.NotFoundException;
 import com.example.productservice.mapper.ProductMapper;
 import com.example.productservice.model.Product;
+import com.example.productservice.producer.ProductProducer;
 import com.example.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
-
+    private final ProductProducer productProducer;
 
     @Transactional
     public Product createProduct(ProductRequest productRequest) {
         Product product = productMapper.toEntity(productRequest);
-        return productRepository.save(product);
+         Product saved = productRepository.save(product);
+         productProducer.sendCreated(saved);
+         return saved;
     }
+
+
 
     @Transactional
     public Product updateProduct(Long id, ProductRequest productRequest) {
         Product updated = productMapper.toEntity(productRequest);
         Product existing = getProductById(id);
         productMapper.merge(existing, updated);
+        productProducer.sendUpdated(existing);
         return existing;
     }
 
@@ -47,11 +52,19 @@ public class ProductService {
     }
 
 
-    @Transactional(readOnly = true)
+    @Transactional
     public void deleteProduct(Long id) {
         if (!productRepository.existsById(id)) {
             throw new NotFoundException("Product not found with id " + id);
         }
         productRepository.deleteById(id);
+        productProducer.sendDeleted(id);
+    }
+
+
+    @Transactional(readOnly = true)
+    public Product getProductByTitle(String title){
+        return productRepository.findProductByTitle(title)
+                .orElseThrow(()-> new NotFoundException("Product with " + title + " not found"));
     }
 }
