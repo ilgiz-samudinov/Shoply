24c87e58de463a7fbc7c0a11f72b664c500f9321
ilgiz <i.samudinov@outlook.com>
2025-12-05 18:10:05 +0600

    feat(product-service): add SubCategory model

--- ProductService/src/main/java/com/example/productservice/model/SubCategory.java
+++ ProductService/src/main/java/com/example/productservice/model/SubCategory.java
@@ -1,4 +1,22 @@
 package com.example.productservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "sub_categories")
 public class SubCategory {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 }
