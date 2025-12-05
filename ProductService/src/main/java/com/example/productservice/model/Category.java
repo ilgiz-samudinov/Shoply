bfc8b2bf9cfb6e1a5009d4053d50a0c7581c0f32
ilgiz <i.samudinov@outlook.com>
2025-12-05 18:09:21 +0600

    feat(product-service): add Category model

---
+++ ProductService/src/main/java/com/example/productservice/model/Category.java
@@ -0,0 +1,26 @@
+package com.example.productservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "sub_categories")
+public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SubCategory> subCategories;
+
+}
